f402a1232cc39edd64c73310dcde714fc7d27a5a
Gautham <ahgamut@gmail.com>
2026-06-14 04:01:10 +0000

    update libffi

--- lib/libffi/BUILD.mk
+++ lib/libffi/BUILD.mk
@@ -1,5 +1,5 @@
 
-LIBFFI_SRC := https://github.com/libffi/libffi/releases/download/v3.4.2/libffi-3.4.2.tar.gz 
+LIBFFI_SRC := https://github.com/libffi/libffi/archive/refs/tags/v3.5.2.tar.gz 
 
 LIBFFI_CONFIG_ARGS = --prefix=$$(COSMOS)\
     --disable-shared --enable-static\
@@ -9,5 +9,12 @@ LIBFFI_CONFIG_ARGS = --prefix=$$(COSMOS)\
 
 $(eval $(call DOWNLOAD_SOURCE,lib/libffi,$(LIBFFI_SRC)))
 $(eval $(call AUTOTOOLS_BUILD,lib/libffi,$(LIBFFI_CONFIG_ARGS),$(LIBFFI_CONFIG_ARGS)))
+
+o/lib/libffi/setup: o/lib/libffi/patched
+	cd $(BASELOC)/o/lib/libffi/libffi* && autoreconf -fi $(ERRLOG)
+	touch $@
+
+o/lib/libffi/configured.x86_64: o/lib/libffi/setup
+o/lib/libffi/configured.aarch64: o/lib/libffi/setup
 
 o/lib/libffi/built.fat: FATTEN_COMMAND = $(DUMMYLINK0)
